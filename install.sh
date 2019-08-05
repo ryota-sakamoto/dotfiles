aceb7cb85ac31c18d34ef7fce67c0ed9a16529c2
Ryota Sakamoto <ryota-sakamoto@gmo.jp>
2019-08-05 15:14:57 +0900

    add requirements

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+for app in `cat requirements`
+do
+    which $app >> /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "You should be install \"$app\""
+    fi
+done
+
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
 
     ln -sf "$(pwd)"/"$f" "$HOME"/"$f"
-    echo "$HOME"/"$f"
 done
 
 ln -sf /root/dotfiles/fish /root/.config
