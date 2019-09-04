93338c6044ffc002821d0aa30a6e15fc4715f4b0
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2019-09-05 00:52:13 +0900

    add go path

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+set -x PATH /Users/ryota/go/bin $PATH
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
